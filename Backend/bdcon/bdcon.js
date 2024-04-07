a30335c31b29844e50c77ef5aedda3364296148e
Dario <adari@gmail.com>
2024-04-07 14:56:15 +0200

    crear bdconexion

---
+++ Backend/bdcon/bdcon.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+var connection= mysql.createConnection({
+    host:"localhost",
+    user:"dbuser",
+    password:"pass"
+});
+
+connection.connect();
+
+connection.end();
+
+export {connection as mysqlConnection};
--- Backend/index..js
+++ Backend/index..js
@@ -1,3 +1,7 @@
+import "controllers/";
+
 const express=require('express');
 const app=express();
 
+app.get("/addUser");
+app.delete("/borrarEjercicio");
