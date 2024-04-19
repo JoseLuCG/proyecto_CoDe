c89fd919dd66c2e5fc99800411fe9e62530dd169
JoseLuCG <joseluisconde@protonmail.com>
2024-04-19 10:55:36 +0200

    Preparing a new endpoint.

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,7 @@ const jsonParser = express.json();
 app.post("/addUser", jsonParser, controllers.addUser);
 app.delete("/removeUser", jsonParser, controllers.removeUser);
 app.post("/checkLogin", jsonParser, controllers.checkLogin);
+
 app.get("/addUserEjercicio", controllers.addUserEjercicio);
 app.get("/getUserCardio", controllers.getUserCardio);
 app.get("/getUserFuerza", controllers.getUserFuerza);
