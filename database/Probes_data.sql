26af26853d6d0056c35b116b7f84d17f19b09c58
JoseLuCG <joseluisconde@protonmail.com>
2024-04-19 10:54:52 +0200

    Add data proves.

--- database/Probes_data.sql
+++ database/Probes_data.sql
@@ -2,3 +2,6 @@ INSERT INTO Users(nameUser, lastNameUser, passwd, phoneNumber)
 	VALUES('Pepe','Rio','abc','600000001');
     
 select logUser('600000001', 'abc') as id;
+
+CALL addStrengthExecise ("Peso Muerto");
+CALL addStrengthExecise ("Bíceps Mancuernas");
