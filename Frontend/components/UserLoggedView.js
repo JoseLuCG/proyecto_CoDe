56ea7929dbe715eab68561c56701182189092068
Dario <adari@gmail.com>
2024-05-15 20:48:33 +0200

    Cambios Vista User Cardio y Fuerza

--- Frontend/components/UserLoggedView.js
+++ Frontend/components/UserLoggedView.js
@@ -1,19 +1,19 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import {styles} from '../styles/Styles.js';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { styles } from '../styles/Styles.js';
 import * as fetchController from '../controllers/fetchController.js';
 
 export function UserLoggedView(props) {
-    
+
     function addCardio(tipo, fecha, intensidadOset, tiempoOpeso, distanciaOrepeticiones) {
-        fetchController.addCardio(props.id,tipo,fecha,intensidadOset,tiempoOpeso,distanciaOrepeticiones)
-        .then(data => {
+        fetchController.addCardio(props.id, tipo, fecha, intensidadOset, tiempoOpeso, distanciaOrepeticiones)
+            .then(data => {
                 props.refreshCardio();
             });
     }
 
     function addFuerza(tipo, fecha, intensidadOset, tiempoOpeso, distanciaOrepeticiones) {
-        fetchController.addFuerza(props.id,tipo,fecha,intensidadOset,tiempoOpeso,distanciaOrepeticiones)
+        fetchController.addFuerza(props.id, tipo, fecha, intensidadOset, tiempoOpeso, distanciaOrepeticiones)
             .then(data => {
                 props.refreshFuerza();
             })
@@ -25,29 +25,31 @@ export function UserLoggedView(props) {
 
     const mappedFuerzas = objetoFuerza.map((obj) => {
         return (
-            <ExerciseFuerza onClickDelete={()=>props.onClickDeleteExercise(props.id,obj.exerciseDate,obj.exerciseName)} id={props.id} exerciseName={obj.exerciseName} exerciseDate={obj.exerciseDate} weight={obj.weight} repeats={obj.repeats} setNumber={obj.setNumber} />
+            <ExerciseFuerza onClickDelete={() => props.onClickDeleteExercise(props.id, obj.exerciseDate, obj.exerciseName)} id={props.id} exerciseName={obj.exerciseName} exerciseDate={obj.exerciseDate} weight={obj.weight} repeats={obj.repeats} setNumber={obj.setNumber} />
         )
     });
 
     const mappedCardio = objetoCardio.map((obj) => {
         return (
             <View>
-                <ExerciseCardio onClickDelete={()=>props.onClickDeleteExercise(props.id,obj.exerciseDate,obj.exerciseName)} id={props.id} exerciseName={obj.exerciseName} exerciseDate={obj.exerciseDate} intensity={obj.intensity} distance={obj.distance} />
+                <ExerciseCardio onClickDelete={() => props.onClickDeleteExercise(props.id, obj.exerciseDate, obj.exerciseName)} id={props.id} exerciseName={obj.exerciseName} exerciseDate={obj.exerciseDate} intensity={obj.intensity} distance={obj.distance} />
             </View>
         )
     });
 
     return (
-        <View>
+        <ScrollView style={styles.Container}>
             <Text>TÚ: {props.name} {props.lastName}, {props.phone}</Text>
             <br />
-            <Text>{mappedFuerzas}</Text>
-            <Text>{mappedCardio}</Text>
+            <h2>Ejercicios de Fuerza: </h2>
+            <View style={styles.items}>{mappedFuerzas}</View>
+            <h2>Ejercicios de Cardio: </h2>
+            <View style={styles.items}>{mappedCardio}</View>
             <AnhadirEjercicioCardioFuerza clickAddCardio={(t, f, i, tt, d) => addCardio(t, f, i, tt, d)} clickAddFuerza={(t, f, i, tt, d) => addFuerza(t, f, i, tt, d)} />
-            <Button title="LOGOUT" onPress={()=>props.onClickLogout()}/>
+            <Button title="LOGOUT" onPress={() => props.onClickLogout()} />
             <br />
-            <Button title="Borrar Usuario" onPress={()=>props.onClickRemoveUser()}/>
-        </View>
+            <Button title="Borrar Usuario" onPress={() => props.onClickRemoveUser()} />
+        </ScrollView>
     );
 }
 
@@ -58,13 +60,14 @@ export function UserLoggedView(props) {
 
 export function ExerciseFuerza(props) {
     return (
-        <View>
+        <View style={styles.item}>
             <Text>Fuerza Tipo: {props.exerciseName}</Text>
             <Text>Fecha: {props.exerciseDate}</Text>
             <Text>Peso: {props.weight}</Text>
             <Text>Repeticiones: {props.repeats}</Text>
             <Text>Numero Sets: {props.setNumber}</Text>
-            <Button title='Eliminar' onPress={()=>{props.onClickDelete()}}/>
+            <Button title='Eliminar' onPress={() => { props.onClickDelete() }} />
+            <br />
         </View>
     );
 }
@@ -76,7 +79,7 @@ export function ExerciseCardio(props) {
             <Text>Fecha: {props.exerciseDate}</Text>
             <Text>Intensidad: {props.intensity}</Text>
             <Text>Distancia: {props.distance}</Text>
-            <Button title="Eliminar" onPress={()=>{props.onClickDelete()}} />
+            <Button title="Eliminar" onPress={() => { props.onClickDelete() }} />
         </View>
     );
 }
--- Frontend/controllers/fetchController.js
+++ Frontend/controllers/fetchController.js
@@ -1,148 +1,157 @@
 /**
      * Fetch Add a new Cardio Exercise to User on Backend/addUserCardioExercise
-     * @param {*} tipo Cardio Type
-     * @param {*} fecha Cardio Date
-     * @param {*} intensidadOset Cardio Intensity
-     * @param {*} tiempoOpeso Cardio Time
-     * @param {*} distanciaOrepeticiones Cardio Distance
+     * @param {String} tipo Cardio Type
+     * @param {Date} fecha Cardio Date
+     * @param {Number} intensidadOset Cardio Intensity
+     * @param {Number} tiempoOpeso Cardio Time
+     * @param {Number} distanciaOrepeticiones Cardio Distance
      */
-    export async function addCardio(id, tipo, fecha, intensidadOset, tiempoOpeso, distanciaOrepeticiones) {
-        const requestOptions = {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ exerciseDate: fecha, idUser: id, exerciseName: tipo, intensity: intensidadOset, exerciseTime: tiempoOpeso, distance: distanciaOrepeticiones })
-        };
-        const response = await fetch("http://localhost:3000/addUserCardioExercise", requestOptions)
-        const data = await response.text();
-        return data;
-    }
+export async function addCardio(id, tipo, fecha, intensidadOset, tiempoOpeso, distanciaOrepeticiones) {
+    const requestOptions = {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ exerciseDate: fecha, idUser: id, exerciseName: tipo, intensity: intensidadOset, exerciseTime: tiempoOpeso, distance: distanciaOrepeticiones })
+    };
+    const response = await fetch("http://localhost:3000/addUserCardioExercise", requestOptions)
+    const data = await response.text();
+    return data;
+}
 
-    /**
-     * Fetch Add a new Strength Exercise to User on Backend/addUserStrengthExercise
-     * @param {*} tipo Strength Type
-     * @param {*} fecha Strength Date
-     * @param {*} intensidadOset Strength Set
-     * @param {*} tiempoOpeso Strength Weight
-     * @param {*} distanciaOrepeticiones Strength Repetitions
-     * @method addStrength
-     */
-    export async function addFuerza(id, tipo, fecha, intensidadOset, tiempoOpeso, distanciaOrepeticiones) {
-        const requestOptions = {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ exerciseDate: fecha, idUser: id, exerciseName: tipo, setNumber: intensidadOset, weight: tiempoOpeso, repeats: distanciaOrepeticiones })
-        };
-        const response=await fetch("http://localhost:3000/addUserStrengthExercise", requestOptions);
-        const data = await response.text();
-        return data;
-    }
+/**
+ * Fetch Add a new Strength Exercise to User on Backend/addUserStrengthExercise
+ * @param {String} tipo Strength Type
+ * @param {Date} fecha Strength Date
+ * @param {Number} intensidadOset Strength Set
+ * @param {Number} tiempoOpeso Strength Weight
+ * @param {Number} distanciaOrepeticiones Strength Repetitions
+ */
+export async function addFuerza(id, tipo, fecha, intensidadOset, tiempoOpeso, distanciaOrepeticiones) {
+    const requestOptions = {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ exerciseDate: fecha, idUser: id, exerciseName: tipo, setNumber: intensidadOset, weight: tiempoOpeso, repeats: distanciaOrepeticiones })
+    };
+    const response = await fetch("http://localhost:3000/addUserStrengthExercise", requestOptions);
+    const data = await response.text();
+    return data;
+}
 
-    /**
-     * Fetch Sends userId to remove from users on Backend/removeUser
-     * @param {*} id userId needed to remove user
-     */
-    export async function removeUser(id) {
-        const requestOptions = {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ idUser: id })
-        };
-        const response=await fetch("http://localhost:3000/removeUser", requestOptions);
-        const data=await response.text();
-        return data;
-    }
+/**
+ * Fetch Sends userId to remove from users on Backend/removeUser
+ * @param {Number} id userId needed to remove user
+ */
+export async function removeUser(id) {
+    const requestOptions = {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ idUser: id })
+    };
+    const response = await fetch("http://localhost:3000/removeUser", requestOptions);
+    const data = await response.text();
+    return data;
+}
 
-    /**
-     * Fetch sends ExerciseDate, idUser and exerciseName to Delete exercise on Backend/removeUserExercise
-     * @param {*} exerciseDate the date of the exercise
-     * @param {*} idUser The Id of actual User
-     * @param {*} exerciseName The name of the Exercise
-     */
-    export async function removeExercise(id, exerciseDate, exerciseName) {
-        const requestOptions = {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ idUser: id, exerciseName: exerciseName, exerciseDate: exerciseDate })
-        };
-        const response = await fetch("http://localhost:3000/removeEjercicio", requestOptions);
-        const data=await response.text();
-        return data;
-    }
+/**
+ * Fetch sends ExerciseDate, idUser and exerciseName to Delete exercise on Backend/removeUserExercise
+ * @param {Date} exerciseDate the date of the exercise
+ * @param {Number} idUser The Id of actual User
+ * @param {String} exerciseName The name of the Exercise
+ */
+export async function removeExercise(id, exerciseDate, exerciseName) {
+    const requestOptions = {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ idUser: id, exerciseName: exerciseName, exerciseDate: exerciseDate })
+    };
+    const response = await fetch("http://localhost:3000/removeEjercicio", requestOptions);
+    const data = await response.text();
+    return data;
+}
 
-     /**
-     * Fetch Sends userID to Backend/getUserCardio and gets Cardio Exercises to update state
-     * @param {*} id ID of logged User
-     */
-     export async function getEjerciciosCardio(id) {
-        const requestOptions = {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ idUser: id })
-        };
-        const response=await fetch("http://localhost:3000/getUserCardio", requestOptions);
-        const data=await response.json();
-        return data;
-    }
+/**
+* Fetch Sends userID to Backend/getUserCardio and gets Cardio Exercises to update state
+* @param {Number} id ID of logged User
+* @returns {Array} Array of User Cardio Objects
+*/
+export async function getEjerciciosCardio(id) {
+    const requestOptions = {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ idUser: id })
+    };
+    const response = await fetch("http://localhost:3000/getUserCardio", requestOptions);
+    const data = await response.json();
+    return data;
+}
 
-    /**
-     * Fetch Sends userID to Backend/getUserStrength and gets Strength Exercises to update state
-     * @param {*} id 
-     */
-    export async function getEjerciciosFuerza(id) {
-        const requestOptions = {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ idUser: id })
-        };
-        const response =await fetch("http://localhost:3000/getUserFuerza", requestOptions);
-        const data=await response.json();
-        return data;
-    }
-    
-    //  Cuando recibe el Login ID, recoge los Datos del Usuario, luego los Cardio y Strength Exercises
-    /**
-     * Fetch Sends Phone and Password to Backend/userLogin and get Login ID to update user Status
-     *  @param {*} passwdIn Password to Attempt to Log In
-     * @param {*} phoneIn Phone to Attempt to 
-     */
-    export async function Login(passwdIn, phoneIn) {
+/**
+ * Fetch Sends userID to Backend/getUserStrength and gets Strength Exercises to update state
+ * @param {Number} id ID of logged user
+ * @returns {Array} Array of User Strength Objects
+ */
+export async function getEjerciciosFuerza(id) {
+    const requestOptions = {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ idUser: id })
+    };
+    const response = await fetch("http://localhost:3000/getUserFuerza", requestOptions);
+    const data = await response.json();
+    return data;
+}
 
-        const response = await fetch("http://localhost:3000/checkLogin", {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ passwd: passwdIn, phoneNumber: phoneIn })
-        })
-        const data=await response.json();
-        return data;
-    }
+//  Cuando recibe el Login ID, recoge los Datos del Usuario, luego los Cardio y Strength Exercises
+/**
+ * Fetch Sends Phone and Password to Backend/userLogin and get Login ID to update user Status
+ *  @param {String} passwdIn Password to Attempt to Log In
+ * @param {String} phoneIn Phone to Attempt to
+ * @returns {Array} Array of 1 or 0 users (only 1, on position 0) 
+ */
+export async function Login(passwdIn, phoneIn) {
 
-    export async function loginNuevo(passwdIn,phoneIn) {
-        const requestOptions={
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ passwd: passwdIn, phoneNumber: phoneIn })
-        };
-        const response = await fetch("http://localhost:3000/getUser",requestOptions);
-        const data= await response.json();
-        return data;
-    }
+    const response = await fetch("http://localhost:3000/checkLogin", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ passwd: passwdIn, phoneNumber: phoneIn })
+    })
+    const data = await response.json();
+    return data;
+}
 
-    /**
-     * Fetch Register a new User on Backend/createUser
-     * @param {*} nombre Name to create User
-     * @param {*} apellidos Surname to create User
-     * @param {*} passwd Password to create User
-     * @param {*} phone Unique Phone to create User (Primary Key)
-     */
-    export async function addUser(nombre, apellidos, passwd, phone) {
-        console.log(nombre);
-        const requestOptions = {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ nameUser: nombre, lastNameUser: apellidos, passwd: passwd, phoneNumber: phone })
-        };
-        const response= await fetch("http://127.0.0.1:3000/addUser", requestOptions)
-        const data=await response.text();
-        return data;
-    }
+//  Cuando recibe el Login ID, recoge los Datos del Usuario, luego los Cardio y Strength Exercises
+/**
+ * Fetch Sends Phone and Password to Backend/userLogin and get Login ID to update user Status
+ *  @param {String} passwdIn Password to Attempt to Log In
+ * @param {String} phoneIn Phone to Attempt to
+ * @returns {Array} Array of 1 or 0 users (only 1, on position 0) 
+ */
+export async function loginNuevo(passwdIn, phoneIn) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ passwd: passwdIn, phoneNumber: phoneIn })
+    };
+    const response = await fetch("http://localhost:3000/getUser", requestOptions);
+    const data = await response.json();
+    return data;
+}
+
+/**
+ * Fetch Register a new User on Backend/createUser
+ * @param {String} nombre Name to create User
+ * @param {String} apellidos Surname to create User
+ * @param {String} passwd Password to create User
+ * @param {String} phone Unique Phone to create User (Primary Key)
+ */
+export async function addUser(nombre, apellidos, passwd, phone) {
+    console.log(nombre);
+    const requestOptions = {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ nameUser: nombre, lastNameUser: apellidos, passwd: passwd, phoneNumber: phone })
+    };
+    const response = await fetch("http://127.0.0.1:3000/addUser", requestOptions)
+    const data = await response.text();
+    return data;
+}
 
