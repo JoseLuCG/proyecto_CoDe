d51eb9418e5f2b2b1b03e8b8d116c03db8c35df8
dario <alejandrodariofo2@gmail.com>
2024-05-21 11:49:51 +0200

    Fix Dependencies and fetchTests JEST

--- Frontend/__tests__/fetchController-test.js
+++ Frontend/__tests__/fetchController-test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import Login from '../components/Login.js';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import * as controllers from '../controllers/fetchController.js'
@@ -12,18 +11,18 @@ test('renders correctly', () => {
 */
 
 test('Funciona el Login', () => {
-    return controllers.Login(12345, 1234)
-        .then((data) => expect(data[0].id).toBe(0));
+    return controllers.loginNuevo(1234, 12345)
+        .then((data) => expect(data[0].id).toBe(13));
 });
 
 test('Funciona el GetCardio', () => {
-    return controllers.getEjerciciosCardio(7)
+    return controllers.getEjerciciosCardio(13)
         .then(data => {
-            expect(data[0].distance).toBe(2)
+            expect(data.idUSer).toBe(13)
         })
 });
 
 test('Funciona el GetFuerza', () => {
-    return controllers.getEjerciciosFuerza(7)
-        .then(data => expect(data[0].idUser).toBe(undefined));
+    return controllers.getEjerciciosFuerza(13)
+        .then(data => expect(data[0].idUSer).toBe(13));
 });
