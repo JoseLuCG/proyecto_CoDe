a252931744f371a31b65ef00fbaa2a685337549d
Jose Luis <condegomesjoseluis@gmail.com>
2025-10-06 18:03:43 +0200

    Feat(Frontend/components): add the animation and the Pressable item

--- Frontend/src/components/NavigationTab.js
+++ Frontend/src/components/NavigationTab.js
@@ -1,11 +1,55 @@
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image, Pressable, Animated } from "react-native";
 import { defaultBRadius } from '../styles/DefaultVaules';
+import { useEffect, useRef } from "react";
 
-function NavigationTab() {
+function NavigationTab({labelText, iconSource, navigateTo, navigation}) {
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+    const scaleAnim = useRef(new Animated.Value(1)).current;
+
+    useEffect(()=> {
+        if (iconSource) {
+            Animated.timing(fadeAnim, {
+                toValue: 1,
+                duration: 500,
+                useNativeDriver: true
+            }).start();
+        }
+    }, [iconSource]);
+
+    const handlePressIn = () => {
+        Animated.spring(scaleAnim, {
+            toValue: 0.75,
+            useNativeDriver: true,
+        }).start();
+    }
+
+    const handlePressOut = () => {
+        Animated.spring(scaleAnim, {
+            toValue: 1,
+            friction: 3,
+            useNativeDriver: true,
+        }).start();
+    };
+
+    function goTo() {
+        console.log("vamonos");
+        
+        //navigation.navigate();
+    }
     return(
-        <View style={styles.navigationTab}>
-            <Text>Home</Text>
-        </View>
+        <Pressable
+            onPress={goTo}
+            onPressIn={handlePressIn}
+            onPressOut={handlePressOut}
+        >
+            <Animated.View style={[styles.navigationTab, { opacity: fadeAnim, transform: [{ scale: scaleAnim }] }]}>
+                <Image
+                    source={iconSource}
+                    style={styles.image}
+                />
+                <Text>{labelText}</Text>
+            </Animated.View>
+        </Pressable>
     );
 }
 
@@ -16,8 +60,15 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0, 200, 255, 0.91)',
         height: 70,
         width: 80,
+        marginLeft: 15,
+        marginRight: 15,
         borderRadius: defaultBRadius,
         alignItems: 'center',
         justifyContent: 'center',
-    }, 
+    },
+    image: {
+        height: 40,
+        width: 60,
+        resizeMode: 'contain',
+    }
 });
