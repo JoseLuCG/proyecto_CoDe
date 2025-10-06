9e1c5d5f853b52aebdc2695f28743f7e15b2dcf4
Jose Luis <condegomesjoseluis@gmail.com>
2025-10-06 17:28:08 +0200

    Feat(): create the array with the logic of navigation tabs

---
+++ Frontend/src/utilities/navitationArrayTab.js
@@ -0,0 +1,17 @@
+export const navigationContentArray = [
+    {
+        labelText: "Exercise",
+        iconSource: require("./../../assets/icons/exercise.png"),
+        navigateTo: "patata"
+    },
+    {
+        labelText: "Feeding",
+        iconSource: require("./../../assets/icons/icons8-rice-bowl-100.png"),
+        navigateTo: "patata"
+    },
+    {
+        labelText: "Overview",
+        iconSource: require("./../../assets/icons/graphic.png"),
+        navigateTo: "patata"
+    },
+];
