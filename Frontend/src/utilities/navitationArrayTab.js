c398c1cdbd5360e131ddbf0a488cb706dcddfce8
Jose Luis <condegomesjoseluis@gmail.com>
2025-10-28 20:19:42 +0100

    Feat(Frontend/utilities): change the `navigateTo` template params for the correct Stack

--- Frontend/src/utilities/navitationArrayTab.js
+++ Frontend/src/utilities/navitationArrayTab.js
@@ -2,16 +2,16 @@ export const navigationContentArray = [
     {
         labelText: "Exercise",
         iconSource: require("./../../assets/icons/exercise.png"),
-        navigateTo: "patata"
+        navigateTo: "Exercises"
     },
     {
         labelText: "Feeding",
         iconSource: require("./../../assets/icons/icons8-rice-bowl-100.png"),
-        navigateTo: "patata"
+        navigateTo: "Feeding"
     },
     {
         labelText: "Overview",
         iconSource: require("./../../assets/icons/graphic.png"),
-        navigateTo: "patata"
+        navigateTo: "Home"
     },
 ];
