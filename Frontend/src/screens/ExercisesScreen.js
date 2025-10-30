1b33590803774c07f60acf12fbd7e06042f36b74
Jose Luis <condegomesjoseluis@gmail.com>
2025-10-30 18:19:12 +0100

    Feat(Frontend/screens): add `AddDataModal` logic to add new exercises

--- Frontend/src/screens/ExercisesScreen.js
+++ Frontend/src/screens/ExercisesScreen.js
@@ -12,6 +12,7 @@ import { colorStyle } from '../styles/Colors';
 import NavigationBar from '../components/NavigationBar';
 import { DaysCarousel } from '../components/DaysCarousel';
 import { defaultBRadius } from '../styles/DefaultVaules';
+import AddDataModal from '../components/Modals/AddDataModal';
 
 const { width } = Dimensions.get('window');
 const menuWidth = 250;
@@ -19,6 +20,11 @@ const menuWidth = 250;
 const ExercisesScreen = ({ navigation }) => { 
     // States:
     const [ selectedDate, setSelectedDate ] = useState(null);
+    const [ addModalVisible, setAddModalVisible ] = useState(false);
+
+    function handleOpenAddModal() {
+        setAddModalVisible(true);
+    }
 
     useEffect(()=> {
         console.log("The day selected is:",selectedDate); // TODO: delete this line when the apps works.
@@ -26,7 +32,12 @@ const ExercisesScreen = ({ navigation }) => {
 
     function openTabToAddExercise() {
         console.log("open!");
-        
+        setAddModalVisible(true);
+    }
+
+    function closeTabToAddExercise() {
+        console.log("close!");
+        setAddModalVisible(false);
     }
 
     return (
@@ -38,9 +49,18 @@ const ExercisesScreen = ({ navigation }) => {
             <View>
                 <Text style={styles.title}>Hello word this is the Exercises page.</Text>
             </View>
+            {/* Button */}
             <TouchableOpacity style={styles.addButton} onPress={openTabToAddExercise}>
                 <Text style={styles.buttonText}>+</Text>
             </TouchableOpacity>
+
+            {/* Modal */}
+            <AddDataModal
+                isVisible={addModalVisible}
+                onClose={closeTabToAddExercise}
+                date={selectedDate}
+            />
+
             <NavigationBar/>
         </LinearGradient>
     );
