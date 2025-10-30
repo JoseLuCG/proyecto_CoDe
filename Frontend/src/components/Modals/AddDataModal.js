213cb18bd305e35115294a4bd678fb2f654a4ee1
Jose Luis <condegomesjoseluis@gmail.com>
2025-10-30 18:21:31 +0100

    Feat(Frontend/components): create the `AddDataModal` component

---
+++ Frontend/src/components/Modals/AddDataModal.js
@@ -0,0 +1,57 @@
+import { View, StyleSheet, Dimensions, Text } from "react-native";
+import Modal from 'react-native-modal';
+
+const { height } = Dimensions.get('window');
+
+export default function AddDataModal({ isVisible, onClose, date }) {
+     return(
+        <Modal
+            isVisible={isVisible}
+            onBackdropPress={onClose}
+            onSwipeComplete={onClose}
+            swipeDirection="down"
+            style={styles.modalContainer}
+            backdropTransitionOutTiming={0}
+            useNativeDriverForBackdrop
+        >
+            <View style={styles.modalContent}>
+                <View style={styles.modalHandle} />
+                <Text style={styles.modalTitle}>
+                    {date ? date.format('DD-MM-YYYY') : 'No date selected'}
+                </Text>
+            </View>
+
+        </Modal>
+     );
+}
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        justifyContent: 'flex-end',
+        margin: 0,
+    },
+    modalContent: {
+        height: height * 0.85,
+        backgroundColor: 'rgba(255, 255, 255, 0.91)',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        padding: 20,
+        alignItems: 'center',
+    },
+    modalHandle: {
+        width: 40,
+        height: 5,
+        backgroundColor: '#ccc',
+        borderRadius: 2.5,
+        marginBottom: 10,
+    },
+    modalTitle: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    modalSubtitle: {
+        fontSize: 18,
+        color: '#666',
+    },
+});
