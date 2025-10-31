11528845e128c7c10d5cff98d7d56aa4714e63b2
Jose Luis <condegomesjoseluis@gmail.com>
2025-10-31 12:07:18 +0100

    Feat(Frontend/screens): add the addButton and the addModal in `FeedingScreen`

--- Frontend/src/screens/FeedingScreen.js
+++ Frontend/src/screens/FeedingScreen.js
@@ -1,36 +1,63 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
     View,
     Text,
     TouchableOpacity,
     StyleSheet,
-    Animated,
     Dimensions,
-    TouchableWithoutFeedback,
 } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { textStyle } from '../styles/TextStyles';
 import { colorStyle } from '../styles/Colors';
-import { buttonStyles } from '../styles/ButtonStyles';
-import SideLeftMenu from '../components/SideLeftMenu';
-import TrainingTab from '../components/TrainingTab';
-import { exampleData } from '../services/dataProves';
-import ExerciseModalScreen from './ExerciseModalScreen';
 import NavigationBar from '../components/NavigationBar';
+import { DaysCarousel } from '../components/DaysCarousel';
+import AddDataModal from '../components/Modals/AddDataModal';
+import { defaultBRadius } from '../styles/DefaultVaules';
 
 const { width } = Dimensions.get('window');
 const menuWidth = 250;
 
-const FeedingScreen = ({ navigation }) => { 
+const FeedingScreen = ({ navigation }) => {
+    // States:
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [addModalVisible, setAddModalVisible] = useState(false);
+
+    // Handlers:
+
+    useEffect(() => {
+        console.log("The day selected is:", selectedDate); // TODO: delete this line when the apps works.
+    }, [selectedDate]);
+
+    function openTabToAddExercise() {
+        console.log("open!");
+        setAddModalVisible(true);
+    }
+
+    function closeTabToAddExercise() {
+        console.log("close!");
+        setAddModalVisible(false);
+    }
+
     return (
         <LinearGradient
             style={styles.mainContainer}
             colors={colorStyle.mainGradient}
         >
+            <DaysCarousel setSelectedDate={setSelectedDate}/>
             <View>
                 <Text style={styles.title}>Hello word this is the Feeding page.</Text>
             </View>
-            <NavigationBar/>
+            {/* Button */}
+            <TouchableOpacity style={styles.addButton} onPress={openTabToAddExercise}>
+                <Text style={styles.buttonText}>+</Text>
+            </TouchableOpacity>
+
+            {/* Modal */}
+            <AddDataModal
+                isVisible={addModalVisible}
+                onClose={closeTabToAddExercise}
+                date={selectedDate}
+            />
+            <NavigationBar />
         </LinearGradient>
     );
 };
@@ -40,73 +67,28 @@ export default FeedingScreen;
 const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         padding: 16,
-    },
-    menuButton: {
-        position: 'absolute',
-        top: 40,
-        left: 20,
-        zIndex: 3,
-        backgroundColor: '#ddd',
-        width: 50,
-        height: 50,
-        borderRadius: 15,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    menuIcon: {
-        fontSize: 24,
-    },
-    overlay: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: width,
-        height: '100%',
-        backgroundColor: 'rgba(0,0,0,0.3)',
-        zIndex: 1,
-    },
-    sideMenu: {
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        width: menuWidth,
-        backgroundColor: '#fff',
-        paddingTop: 80,
-        paddingHorizontal: 20,
-        zIndex: 2,
-        elevation: 5,
-        shadowColor: '#000',
-        shadowOpacity: 0.3,
-        shadowOffset: { width: 2, height: 0 },
-        shadowRadius: 5,
-        borderTopRightRadius: 20,
-        borderBottomRightRadius: 20,
-    },
-    menuItem: {
-        paddingVertical: 15,
-        borderBottomWidth: 1,
-        borderBottomColor: '#eee',
-    },
-    menuText: {
-        fontSize: 18,
-        fontFamily: "main-font"
-    },
-    content: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
     title: {
         fontSize: 22,
         fontWeight: 'bold',
+        top: 50
     },
-    routinesContainer: {
-        backgroundColor: 'rgba(201, 237, 255, 0.76)',
-        height: 200,
-        width: 300,
-        borderRadius: 14,
+    addButton: {
+        position: 'absolute',
+        bottom: 140,
+        right: 20,
+        zIndex: 3,
+        backgroundColor: '#ddd',
+        width: 50,
+        height: 50,
+        borderRadius: defaultBRadius,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }, buttonText: {
+        fontSize: 24,
+        fontWeight: 'bold'
     }
 });
