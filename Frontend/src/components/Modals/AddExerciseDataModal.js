345615b015cdec38510ef41a657ce50780400793
Jose Luis <condegomesjoseluis@gmail.com>
2025-11-02 23:54:45 +0100

    Feat(Frontend/components): enhance `AddExerciseDataModal` with `WorkoutSwitch` and dynamic input handling
    - Replaced View with ScrollView for better layout and scrolling support
    - Added WorkoutSwitch component for exercise type selection
    - Introduced handleInputChange function to manage input updates dynamically
    - Expanded exercise data structure with cardio and strength exercise fields
    - Updated styling to use screen width for responsive layout

--- Frontend/src/components/Modals/AddExerciseDataModal.js
+++ Frontend/src/components/Modals/AddExerciseDataModal.js
@@ -1,12 +1,15 @@
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet, Dimensions, ScrollView } from "react-native";
 import InputField from "../InputField";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import WorkoutSwitch from "../WorkoutSwitch";
+
+const { width } = Dimensions.get('window');
 
 export default function AddExerciseDataModal() {
     // States:
     const [ exerciseData, setExerciseData ] = useState({
         exerciseName: "",
-        exerciseType: 1,
+        exerciseType: false,
         exerciseDate: "",
         exerciseIntensity: "",
         exerciseTime: "",
@@ -16,19 +19,74 @@ export default function AddExerciseDataModal() {
         exerciseRepeats: ""
     });
 
+    // Handlers:
+    function handleInputChange(fieldName, value) {
+        setExerciseData(prevState => ({
+            ...prevState,
+            [fieldName]:value
+        }));
+    }
+
     return(
-        <View>
+        <ScrollView style={styles.container}>
     		<InputField
 				label="Exercise name:"
-				value={userToLogIn.userLoginData}
-				onChangeText={(text) => handleinputChange("exerciseName", text)}
+				value={exerciseData.exerciseName}
+				onChangeText={(text) => handleInputChange("exerciseName", text)}
 				keyboardType="phone-pad"
 			/>
-            
-        </View>
+
+            <WorkoutSwitch 
+                exerciseType={exerciseData.exerciseType} 
+                setExerciseType={ (value) => handleInputChange('exerciseType', value)}
+            />
+
+            {/* Cardio Exercises */}
+            <InputField
+				label="Time:"
+				value={exerciseData.exerciseTime}
+				onChangeText={(text) => handleInputChange("exerciseName", text)}
+				keyboardType="phone-pad"
+			/>
+            <InputField
+				label="Distance:"
+				value={exerciseData.exerciseDistance}
+				onChangeText={(text) => handleInputChange("exerciseName", text)}
+				keyboardType="phone-pad"
+			/>
+            <InputField
+				label="Intensity:"
+				value={exerciseData.exerciseIntensity}
+				onChangeText={(text) => handleInputChange("exerciseName", text)}
+				keyboardType="phone-pad"
+			/>
+
+            {/* Strength Exercises */}
+            <InputField
+				label="Weight:"
+				value={exerciseData.exerciseWeight}
+				onChangeText={(text) => handleInputChange("exerciseName", text)}
+				keyboardType="phone-pad"
+			/>
+            <InputField
+				label="Repeats:"
+				value={exerciseData.exerciseRepeats}
+				onChangeText={(text) => handleInputChange("exerciseName", text)}
+				keyboardType="phone-pad"
+			/>
+            <InputField
+				label="Add new set"
+				value={exerciseData.exerciseSetNumber}
+				onChangeText={(text) => handleInputChange("exerciseName", text)}
+				keyboardType="phone-pad"
+			/>
+
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        width: width * 0.80,
+    }
 });
