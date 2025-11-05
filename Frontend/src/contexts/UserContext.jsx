021fd4248408c595dc7c1899af1a3324b19a5dc7
Jose Luis <condegomesjoseluis@gmail.com>
2025-11-05 19:19:25 +0100

    Feat(Frontend/contexts): persist user state with storage utility in UserProvider
    Integrated the Storage utility into UserProvider to
    enable persistent user sessions.
    - Loads stored user data on mount using loadUser()
    - Saves or removes user data on state changes via saveUser()
    - Uses AsyncStorage or localStorage depending on platform
    
    Improves user experience by maintaining login state across app restarts.

--- Frontend/src/contexts/UserContext.jsx
+++ Frontend/src/contexts/UserContext.jsx
@@ -1,9 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import Storage from "../utilities/storagePersistence";
 
 export const User = createContext();
 
 export function UserProvider({ children }) {
     const [user, setUser] = useState(null);
+
+    async function loadUser() {
+        const storedUser= await Storage.getItem("user");
+        if (storedUser) setUser(JSON.parse(storedUser));
+    }
+
+    async function saveUser() {
+        if(user !== null) {
+            await Storage.setItem("user", JSON.stringify(user));
+        } else {
+            await Storage.removeItem("user");
+        }
+    }
+
+    useEffect(()=> {
+        loadUser();
+    }, []);
+
+    useEffect(()=> {
+        saveUser();
+    }, [user]);
 
     return(
         <User.Provider value={[user, setUser]}>
