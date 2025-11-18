674db37ae624461ff1bc2ce69d55998a12cbcc6d
Jose Luis <condegomesjoseluis@gmail.com>
2025-11-18 19:38:14 +0100

    Feat(Frontend/services): implement the `exerciseTypeChecker` in the service to send the correct information

--- Frontend/src/services/exerciseService.js
+++ Frontend/src/services/exerciseService.js
@@ -1,8 +1,11 @@
+import { exerciseTypeChecker } from "../adapters/exerciseAdapters";
+
 export async function addCardioExercise(newData) {
+    const adaptedData = exerciseTypeChecker(newData);
     const fetchOptions = {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newData) 
+        body: JSON.stringify(adaptedData) 
     };
     // const response = await fetch("http://192.168.1.132:3000/addUser", fetchOptions);
     const response = await fetch("http://192.168.1.130:3000/record-cardio-exercise", fetchOptions);
