6ab231c60f2da0a3958a016ed3cb7da3a0111ed6
Jose Luis <condegomesjoseluis@gmail.com>
2025-12-14 15:01:43 +0100

    Feat(Frontend/utilities): update the `apiRoutes` and create the `HOST_IP`

--- Frontend/src/utilities/defineConfig.js
+++ Frontend/src/utilities/defineConfig.js
@@ -1,21 +1,18 @@
-const PC = "windows";
-const ARCH_HOST_IP = "http://192.168.1.132:3000";
-const WINDOWS_HOST_IP = "http://192.168.1.130:3000";
-
-let IpAddresshostOS;
-
-if (PC == "arch") {
-    IpAddresshostOS = ARCH_HOST_IP;
-} else {
-    IpAddresshostOS = WINDOWS_HOST_IP;
-}
-
+const HOST_IP = "http://192.168.1.134:3000";
 const apiRoutes = {
-    addUser : "/addUser",
+    user: {
+        addUser : "/addUser",
+    },
+    exercise: {
+        cardio: {
+            addExercise: "/record-cardio-exercise",
+            getCardioExercisesInDate: "/cardio-exercises/"
+        }
+    }
 
 }
 
 export {
-    IpAddresshostOS,
+    HOST_IP,
     apiRoutes
 }
