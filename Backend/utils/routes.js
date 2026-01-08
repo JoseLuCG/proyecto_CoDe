f4e2ec1118a50eebd450b7cd3379718a3dae9b1e
Jose Luis <condegomesjoseluis@gmail.com>
2026-01-08 22:35:40 +0100

    Feat(Backend/utils): add the strenght routes statement

--- Backend/utils/routes.js
+++ Backend/utils/routes.js
@@ -6,3 +6,8 @@ export const cardioRoutes = {
     addCardioExercise: "/record-cardio-exercise",
     getCardioExercisesInDate: "/cardio-exercises/:date/:user"
 }
+
+export const strenghtRoutes = {
+    addStrengthExercise: "/record-strength-exercise",
+    getCardioExercisesInDate: "/strength-exercises/:date/:user"
+}
