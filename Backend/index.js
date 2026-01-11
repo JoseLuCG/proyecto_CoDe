c8e0069b1b911819eb50f1c625ac0a7cadcd7d7a
Jose Luis <condegomesjoseluis@gmail.com>
2026-01-11 18:47:02 +0100

    Refactor(Backend/index): remove unnecessary code and add the necessary TODOs

--- Backend/index.js
+++ Backend/index.js
@@ -20,25 +20,20 @@ app.use(cors(corsOptions));
 // * User End-points:
 app.post("/addUser", jsonParser, userControllers.addNewUser);
 app.post("/checkLogin", jsonParser, userControllers.checkLogin); 
+// TODO: Add endpoint to update the user
+// TODO: Add endpoint to recover the password
+// TODO: Add endpoint to delete users
 
 // * Cardio End-points
 app.post(cardioRoutes.addCardioExercise, jsonParser, cardioControllers.addCardioExercise);
 app.get(cardioRoutes.getCardioExercisesInDate, cardioControllers.getCardioExercises);
+// TODO: Add endpoint to update the exercise
+// TODO: Add endpoint to delete the exercise
 
 // * Strength End-points
 app.post(strenghtRoutes.addStrengthExercise, jsonParser, strengthControllers.addStrengthExecise);
-
-
-app.post("/removeUser", jsonParser, controllers.removeUser); 
-app.post("/addStrengthExercise", jsonParser, controllers.addStrengthExercise); 
-//app.post("/addCardioExercise", jsonParser, controllers.addCardioExercise); 
-app.post("/addUserStrengthExercise", jsonParser, controllers.addUserStrengthExercise); 
-//app.post("/addUserCardioExercise", jsonParser, controllers.addUserCardioExercise); 
-
-app.post("/getUserCardio", jsonParser, controllers.getUserCardio);
-app.post("/getUserFuerza", jsonParser, controllers.getUserFuerza);
-app.post("/removeEjercicio", jsonParser, controllers.removeUserEjercicio); 
-app.post("/getUser",jsonParser,controllers.getUser);
+// TODO: Add endpoint to update the exercise
+// TODO: Add endpoint to delete the exercise
 
 app.use("/test", controllers.testPruebas);
 
