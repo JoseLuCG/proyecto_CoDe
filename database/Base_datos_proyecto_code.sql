e54fd8c70df45697e60effa189cb60f7f16f177b
Jose Luis <condegomesjoseluis@gmail.com>
2026-01-22 17:23:39 +0100

    Feat(database): change the database of the proyect
    Changed the database split by separated the strength training
    table in `exercise_set` and `strength_exercise`.

--- database/Base_datos_proyecto_code.sql
+++ database/Base_datos_proyecto_code.sql
@@ -41,6 +41,54 @@ CREATE TABLE cardio_exercise (
     PRIMARY KEY (exercise_date, uuid_user, exercise_name),
     FOREIGN KEY (uuid_user) REFERENCES Users (uuid_user) ON UPDATE CASCADE
 );
+-- New Tables
+CREATE TABLE strength_exercise (
+    uuid_strength_exercise CHAR(36) NOT NULL,
+    exercise_date DATE NOT NULL,
+    uuid_user CHAR(36) NOT NULL,
+    exercise_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (uuid_strength_exercise),
+    FOREIGN KEY (uuid_user)
+        REFERENCES Users (uuid_user)
+        ON UPDATE CASCADE,
+    UNIQUE (exercise_date, uuid_user, exercise_name)
+);
+
+CREATE TABLE exercise_set (
+    uuid_exercise_set CHAR(36) NOT NULL,
+    uuid_strength_exercise CHAR(36) NOT NULL,
+    set_number INTEGER NOT NULL,
+    weight DOUBLE NOT NULL,
+    repeats INTEGER NOT NULL,
+    PRIMARY KEY (uuid_exercise_set),
+    FOREIGN KEY (uuid_strength_exercise)
+        REFERENCES strength_exercise (uuid_strength_exercise)
+        ON DELETE CASCADE,
+    CHECK (weight >= 0),
+    CHECK (repeats > 0),
+    CHECK (set_number > 0),
+    UNIQUE (uuid_strength_exercise, set_number)
+);
+-- Data Migration
+INSERT INTO strength_exercise ( uuid_strength_exercise, exercise_date, uuid_user, exercise_name)
+SELECT UUID(), exercise_date, uuid_user, exercise_name
+FROM (
+    SELECT DISTINCT
+        exercise_date,
+        uuid_user,
+        exercise_name
+    FROM strength_exercise_old
+) t;
+
+INSERT INTO exercise_set (uuid_exercise_set, uuid_strength_exercise, set_number, weight, repeats)
+SELECT
+    UUID(), se.uuid_strength_exercise, old.set_number, old.weight, old.repeats
+	FROM strength_exercise_old old
+		JOIN strength_exercise se
+			ON se.exercise_date = old.exercise_date
+			AND se.uuid_user = old.uuid_user
+			AND se.exercise_name = old.exercise_name
+;
 
 -- Tables updates --
 /* Cardio table */
@@ -54,6 +102,8 @@ ALTER TABLE strength_exercise
 DROP PRIMARY KEY;
 ALTER TABLE strength_exercise
 ADD PRIMARY KEY (uuid_strength_exercise);
+ALTER TABLE strength_exercise
+RENAME TO strength_exercise_old;
 
 /* Users Table */
 ALTER TABLE users
@@ -62,3 +112,11 @@ ALTER TABLE users
 ADD COLUMN weight FLOAT;
 ALTER TABLE users
 ADD COLUMN height FLOAT;
+
+SELECT
+    se.exercise_name,
+    COUNT(es.uuid_exercise_set) AS total_sets
+FROM strength_exercise se
+JOIN exercise_set es
+  ON se.uuid_strength_exercise = es.uuid_strength_exercise
+GROUP BY se.uuid_strength_exercise;
