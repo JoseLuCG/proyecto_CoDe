c64dafb2ecdb8326482b8ea0ac677c8a995db965
Jose Luis <condegomesjoseluis@gmail.com>
2026-01-23 15:49:35 +0100

    Refactor(Frontend/services): update the authservice
    Imported the `HOST_IP` and `apiRoutes` statements to
    to facilitate the configurations in the future. Furthermore
    unnecessary comments were removed.

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,19 +1,16 @@
 import { userFromBackToFront } from "../mappers/userMappers";
+import { HOST_IP, apiRoutes } from "../utilities/defineConfig";
 
 export async function loginUser(userData) {
+    const apiEndPointDirection = HOST_IP + apiRoutes.user.singUp;
 	const fetchOptions = {
 		method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(userData)
     };
-	//const response = await fetch("http://192.168.1.132:3000/checkLogin", fetchOptions);
-    const response = await fetch("http://192.168.1.130:3000/checkLogin", fetchOptions);
+    const response = await fetch(apiEndPointDirection, fetchOptions);
     const jsonData = await response.json();
     const data = userFromBackToFront(jsonData);
     
     return data;
 };
-
-// 192.168.1.132/24 - Android.
-
-// http://127.0.0.1:3000 - Localhost.
